3170da825a782136b7aa83723f34268358cd6b4e
LikiBlaze <LikiBlaze@foxmail.com>
2020-10-29 16:50:29 +0800

    ROSMONTIS!!!

---
+++ CSPAC277.cpp
@@ -0,0 +1,176 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+inline long long read()
+{
+    long long x = 0;
+    int f = 1;
+    char ch = getchar();
+    while (ch < '0' || ch > '9')
+    {
+        if (ch == '-')
+            f = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9')
+    {
+        x = (x << 1) + (x << 3) + (ch ^ 48);
+        ch = getchar();
+    }
+    return x * f;
+}
+void write(const long long &x)
+{
+    if (!x)
+    {
+        putchar('0');
+        return;
+    }
+    char f[100];
+    long long tmp = x;
+    if (tmp < 0)
+    {
+        tmp = -tmp;
+        putchar('-');
+    }
+    long long s = 0;
+    while (tmp > 0)
+    {
+        f[s++] = tmp % 10 + '0';
+        tmp /= 10;
+    }
+    while (s > 0)
+    {
+        putchar(f[--s]);
+    }
+}
+inline double dread()
+{
+    double r;
+    double x = 0, t = 0;
+    int s = 0, f = 1;
+    char c = getchar();
+    for (; !isdigit(c); c = getchar())
+    {
+        if (c == '-')
+        {
+            f = -1;
+        }
+        if (c == '.')
+        {
+            goto readt;
+        }
+    }
+    for (; isdigit(c) && c != '.'; c = getchar())
+    {
+        x = x * 10 + c - '0';
+    }
+readt:
+    for (; c == '.'; c = getchar())
+        ;
+    for (; isdigit(c); c = getchar())
+    {
+        t = t * 10 + c - '0';
+        ++s;
+    }
+    r = (x + t / pow(10, s)) * f;
+    return r;
+}
+
+inline void dwrite(long long x)
+{
+    if (x == 0)
+    {
+        putchar(48);
+        return;
+    }
+    int bit[20], p = 0, i;
+    for (; x; x /= 10)
+        bit[++p] = x % 10;
+    for (i = p; i > 0; --i)
+        putchar(bit[i] + 48);
+}
+inline void write(double x, int k)
+{
+    static int n = pow(10, k);
+    if (x == 0)
+    {
+        putchar('0');
+        putchar('.');
+        for (int i = 1; i <= k; ++i)
+            putchar('0');
+        return;
+    }
+    if (x < 0)
+        putchar('-'), x = -x;
+    long long y = (long long)(x * n) % n;
+    x = (long long)x;
+    dwrite(x), putchar('.');
+    int bit[10], p = 0, i;
+    for (; p < k; y /= 10)
+        bit[++p] = y % 10;
+    for (i = p; i > 0; i--)
+        putchar(bit[i] + 48);
+}
+
+int totN;
+int totM;
+
+struct Edge
+{
+	int val;
+	int nxt;
+	int to;
+}edges[100090];
+int head[100090];
+int cnt_edges,cnt_notedges;
+int dfn[100090];
+bool ins[100090];
+vector<int> SCC;
+
+
+void add_edge(int x,int y,int z)
+{
+	cnt_edges++;
+	edges[cnt_edges].nxt=head[x];
+	head[x]=cnt_edges;
+	edges[cnt_edges].to=y;
+	edges[cnt_edges].val=z;
+}
+bool check(long long checknum)
+{
+	
+}
+void Tarjian(int nowX)
+{
+	
+}
+
+long long L,R=9999999999;
+long long Mid;
+
+int main()
+{
+	totN=read();
+	totM=read();
+	for(int i=1;i<=totM;++i)
+	{
+		int x=read();
+		int y=read();
+		int z=read();
+		add_edge(x,y,z);
+		add_notedge(y,x,z);
+	}
+	while(L<=R)
+	{
+		Mid=(L+R)>>2;
+		if(check(Mid))
+		{
+			
+		}
+	}
+	
+    return 0;
+} //LikiBlaze Code
+
