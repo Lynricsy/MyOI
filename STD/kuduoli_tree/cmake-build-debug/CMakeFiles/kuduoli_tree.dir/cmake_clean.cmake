c6d9aa4b3b1d1a47c3985399ac29afe8c50b8950
Thomitics <i@foxex.cn>
2021-04-27 20:41:13 +0800

    Tech YYZ 做事

---
+++ STD/kuduoli_tree/cmake-build-debug/CMakeFiles/kuduoli_tree.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kuduoli_tree.dir/main.cpp.obj"
+  "kuduoli_tree.exe"
+  "kuduoli_tree.exe.manifest"
+  "kuduoli_tree.lib"
+  "kuduoli_tree.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kuduoli_tree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
