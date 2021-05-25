fc2a784a94526dbf1d8dd210676829943d929068
Thomitics <i@foxex.cn>
2021-05-25 08:49:58 +0800

    oh

---
+++ others/20210517B.cpp
@@ -0,0 +1,160 @@
+#include <bits/stdc++.h>
+#define INF 999999999
+
+using namespace std;
+
+inline long long read()
+{
+    long long x = 0;
+    int f = 1;
+    char ch = getchar();
+    while (ch < '0' || ch > '9')
+    {
+        if (ch == '-')
+            f = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9')
+    {
+        x = (x << 1) + (x << 3) + (ch ^ 48);
+        ch = getchar();
+    }
+    return x * f;
+}
+void write(const long long &x)
+{
+    if (!x)
+    {
+        putchar('0');
+        return;
+    }
+    char f[100];
+    long long tmp = x;
+    if (tmp < 0)
+    {
+        tmp = -tmp;
+        putchar('-');
+    }
+    long long s = 0;
+    while (tmp > 0)
+    {
+        f[s++] = tmp % 10 + '0';
+        tmp /= 10;
+    }
+    while (s > 0)
+    {
+        putchar(f[--s]);
+    }
+}
+inline double dread()
+{
+    double r;
+    double x = 0, t = 0;
+    int s = 0, f = 1;
+    char c = getchar();
+    for (; !isdigit(c); c = getchar())
+    {
+        if (c == '-')
+        {
+            f = -1;
+        }
+        if (c == '.')
+        {
+            goto readt;
+        }
+    }
+    for (; isdigit(c) && c != '.'; c = getchar())
+    {
+        x = x * 10 + c - '0';
+    }
+readt:
+    for (; c == '.'; c = getchar())
+        ;
+    for (; isdigit(c); c = getchar())
+    {
+        t = t * 10 + c - '0';
+        ++s;
+    }
+    r = (x + t / pow(10, s)) * f;
+    return r;
+}
+
+inline void dwrite(long long x)
+{
+    if (x == 0)
+    {
+        putchar(48);
+        return;
+    }
+    int bit[20], p = 0, i;
+    for (; x; x /= 10)
+        bit[++p] = x % 10;
+    for (i = p; i > 0; --i)
+        putchar(bit[i] + 48);
+}
+inline void write(double x, int k)
+{
+    static int n = pow(10, k);
+    if (x == 0)
+    {
+        putchar('0');
+        putchar('.');
+        for (int i = 1; i <= k; ++i)
+            putchar('0');
+        return;
+    }
+    if (x < 0)
+        putchar('-'), x = -x;
+    long long y = (long long)(x * n) % n;
+    x = (long long)x;
+    dwrite(x), putchar('.');
+    int bit[10], p = 0, i;
+    for (; p < k; y /= 10)
+        bit[++p] = y % 10;
+    for (i = p; i > 0; i--)
+        putchar(bit[i] + 48);
+}
+
+long long totN;
+long long totM;
+long long val[100090];
+long long DP[100090];
+struct Node
+{
+    int time;
+    int val;
+}Q[100090];
+long long head, tail;
+long long totMIN=99999999;
+
+int main()
+{
+    totN = read();
+    totM = read();
+    for (int i = 1; i <= totN; ++i)
+    {
+        val[i] = read();
+    }
+    memset(DP, 0x3f, sizeof(DP));
+    head=tail=1;
+    for (int i = 1; i <= totN; ++i)
+    {
+        if(Q[head].time<i-totM)
+        {
+            ++head;
+        }
+        DP[i] = Q[head].val+val[i];
+        while((head < tail)&&(Q[head].val)>DP[i])
+        {
+            --tail;
+        }
+        Q[tail].val=DP[i];
+        Q[tail].time=i;
+    }
+    for (int i = totN-totM ; i <= totN; i++)
+    {
+        totMIN=min(totMIN,DP[i]);
+    }
+    write(totMIN);
+    return 0;
+} //Thomitics Code
