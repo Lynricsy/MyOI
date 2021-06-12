448e9f6bcd329f694efcda27260d5fbddfe0004d
Thomitics <i@foxex.cn>
2021-06-12 15:49:36 +0800

    azhe

---
+++ Luogu/P5410.cpp
@@ -0,0 +1,157 @@
+#include <bits/stdc++.h>
+#define INF 999999999
+
+using namespace std;
+
+inline long long read()
+{
+    long long x = 0;
+    int f = 1;
+    char ch = getchar();
+    while (ch < '0' || ch > '9')
+    {
+        if (ch == '-')
+            f = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9')
+    {
+        x = (x << 1) + (x << 3) + (ch ^ 48);
+        ch = getchar();
+    }
+    return x * f;
+}
+void write(const long long &x)
+{
+    if (!x)
+    {
+        putchar('0');
+        return;
+    }
+    char f[100];
+    long long tmp = x;
+    if (tmp < 0)
+    {
+        tmp = -tmp;
+        putchar('-');
+    }
+    long long s = 0;
+    while (tmp > 0)
+    {
+        f[s++] = tmp % 10 + '0';
+        tmp /= 10;
+    }
+    while (s > 0)
+    {
+        putchar(f[--s]);
+    }
+}
+inline double dread()
+{
+    double r;
+    double x = 0, t = 0;
+    int s = 0, f = 1;
+    char c = getchar();
+    for (; !isdigit(c); c = getchar())
+    {
+        if (c == '-')
+        {
+            f = -1;
+        }
+        if (c == '.')
+        {
+            goto readt;
+        }
+    }
+    for (; isdigit(c) && c != '.'; c = getchar())
+    {
+        x = x * 10 + c - '0';
+    }
+readt:
+    for (; c == '.'; c = getchar())
+        ;
+    for (; isdigit(c); c = getchar())
+    {
+        t = t * 10 + c - '0';
+        ++s;
+    }
+    r = (x + t / pow(10, s)) * f;
+    return r;
+}
+
+inline void dwrite(long long x)
+{
+    if (x == 0)
+    {
+        putchar(48);
+        return;
+    }
+    int bit[20], p = 0, i;
+    for (; x; x /= 10)
+        bit[++p] = x % 10;
+    for (i = p; i > 0; --i)
+        putchar(bit[i] + 48);
+}
+inline void write(double x, int k)
+{
+    static int n = pow(10, k);
+    if (x == 0)
+    {
+        putchar('0');
+        putchar('.');
+        for (int i = 1; i <= k; ++i)
+            putchar('0');
+        return;
+    }
+    if (x < 0)
+        putchar('-'), x = -x;
+    long long y = (long long)(x * n) % n;
+    x = (long long)x;
+    dwrite(x), putchar('.');
+    int bit[10], p = 0, i;
+    for (; p < k; y /= 10)
+        bit[++p] = y % 10;
+    for (i = p; i > 0; i--)
+        putchar(bit[i] + 48);
+}
+
+char A[100090], B[100090];
+int totN;
+int totM;
+int Z[100090], P[100090];
+
+inline void Zfun()
+{
+    for (int i = 1; i <= totN; ++i)
+    {
+        Z[i] = 0;
+    }
+    Z[1] = totN;
+    for (int i = 1, l = 0, r = 0; i <= totN; ++i)
+    {
+        if (i<=r)
+        {
+            Z[i]=min(l+Z[i]-1,r-i+1);
+        }
+        while(i+Z[i]<=totN&&A[i+Z[i]]==A[Z[i]+1])
+        {
+            ++Z[i];
+        }
+        if(l+Z[i]-1>r)
+        {
+            l=i;
+            r=l+Z[i]-1;
+        }
+    }
+}
+inline void
+
+int main()
+{
+    scanf("%s", A + 1);
+    scanf("%s", B + 1);
+    totN = strlen(A);
+    totM = strlen(B);
+
+    return 0;
+} //Thomitics Code
